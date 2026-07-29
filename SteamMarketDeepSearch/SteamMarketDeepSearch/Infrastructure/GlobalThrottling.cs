1129fdb0d99c94bfc8be3ea165f7c05b471d5dbd
HybridDeath <hyperek635@gmail.com>
2026-07-29 21:03:44 +0200

    Double delay for more leeway

---
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Infrastructure/GlobalThrottling.cs
@@ -0,0 +1,12 @@
+﻿namespace SteamMarketDeepSearch.Infrastructure
+{
+    public static class GlobalThrottling
+    {
+        public static readonly int MarketIndexDelayMs = 15000;
+
+        public static int GetMarketIndexDelay()
+        {
+            return MarketIndexDelayMs;
+        }
+    }
+}
