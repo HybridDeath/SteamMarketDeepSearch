c4a943b42f1953adab962408d06b7af768ee3d01
HybridDeath <hyperek635@gmail.com>
2026-07-29 22:24:24 +0200

    Renamed from ScannerService

---
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Services/ListingScannerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamMarketDeepSearch.Services
+{
+    public class ListingScannerService
+    {
+        public bool IsRunning { get; private set; }
+
+        public void Start()
+        {
+            IsRunning = true;
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+        }
+    }
+}
