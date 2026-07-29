acbd061ffa749b0628d53928de09044bd8f72248
HybridDeath <hyperek635@gmail.com>
2026-07-29 16:32:16 +0200

    Prepare for L1 parser replace

--- SteamMarketDeepSearch/SteamMarketDeepSearch/Parsers/SteamMarketParser.cs
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Parsers/SteamMarketParser.cs
@@ -1,5 +1,6 @@
 ﻿using SteamMarketDeepSearch.Models;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace SteamMarketDeepSearch.Parsers
@@ -10,6 +11,14 @@ namespace SteamMarketDeepSearch.Parsers
             GenerateHashNameRegex();
 
 
+        private static readonly Regex SellOrderRegex =
+            GenerateSellOrderRegex();
+
+
+        private static readonly Regex BucketIdRegex =
+            GenerateBucketIdRegex();
+
+
         public static List<MarketSkinData> ParseListings(string html)
         {
             List<MarketSkinData> results = [];
@@ -17,11 +26,65 @@ namespace SteamMarketDeepSearch.Parsers
 
             foreach (Match match in HashNameRegex.Matches(html))
             {
+                int start =
+                    html.LastIndexOf(
+                        @"\""strHash\"":",
+                        match.Index);
+
+
+                if (start < 0)
+                {
+                    start = match.Index;
+                }
+
+
+                int end =
+                    html.IndexOf(
+                        @"}},\""app\"":",
+                        match.Index);
+
+
+                if (end < 0)
+                {
+                    end =
+                        html.Length;
+                }
+
+
+                string fragment =
+                    html[start..end];
+
+
+                Match sellMatch =
+                    SellOrderRegex.Match(fragment);
+
+
+                Match bucketMatch =
+                    BucketIdRegex.Match(fragment);
+
+
                 results.Add(
                     new MarketSkinData
                     {
                         MarketHashName =
-                            match.Groups[1].Value
+                            match.Groups[1].Value,
+
+
+                        SellOrderCount =
+                            sellMatch.Success
+                            ?
+                            int.Parse(
+                                sellMatch.Groups[1].Value)
+                            :
+                            0,
+
+
+                        MarketBucketId =
+                            bucketMatch.Success
+                            ?
+                            bucketMatch.Groups[1].Value
+                            :
+                            string.Empty
                     });
             }
 
@@ -29,10 +92,21 @@ namespace SteamMarketDeepSearch.Parsers
             return results;
         }
 
-
         [GeneratedRegex(
             @"market_hash_name\\\\\\?""?\\\\?""?[:\\]+\\\\?""?([^""\\]+)",
             RegexOptions.Compiled)]
         private static partial Regex GenerateHashNameRegex();
+
+
+        [GeneratedRegex(
+            @"cSellOrders\\+""?:(\d+)",
+            RegexOptions.Compiled)]
+        private static partial Regex GenerateSellOrderRegex();
+
+
+        [GeneratedRegex(
+            @"market_bucket_group_id\\+.*?([A-Z0-9]{10,})",
+            RegexOptions.Compiled)]
+        private static partial Regex GenerateBucketIdRegex();
     }
 }
