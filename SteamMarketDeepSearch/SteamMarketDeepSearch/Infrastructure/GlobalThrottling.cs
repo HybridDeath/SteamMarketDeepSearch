ff4b25306b817e2705841564ddc5f5284dc6af0f
HybridDeath <hyperek635@gmail.com>
2026-07-29 22:31:30 +0200

    Add L2 delay

--- SteamMarketDeepSearch/SteamMarketDeepSearch/Infrastructure/GlobalThrottling.cs
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Infrastructure/GlobalThrottling.cs
@@ -3,10 +3,16 @@
     public static class GlobalThrottling
     {
         public static readonly int MarketIndexDelayMs = 15000;
+        public static readonly int MarketScanDelayMs = 15000;
 
         public static int GetMarketIndexDelay()
         {
             return MarketIndexDelayMs;
+        }
+
+        public static int GetMarketScanDelay()
+        {
+            return MarketScanDelayMs;
         }
     }
 }
