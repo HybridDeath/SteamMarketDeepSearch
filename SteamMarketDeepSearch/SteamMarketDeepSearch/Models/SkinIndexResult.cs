d3297a9a3000d191384a2e532d435a6d25b8a3d6
HybridDeath <hyperek635@gmail.com>
2026-07-29 16:33:49 +0200

    Fix return type in index

---
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Models/SkinIndexResult.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SteamMarketDeepSearch.Models
+{
+    public class SkinIndexResult
+    {
+        public List<SkinDefinition> Skins { get; set; } = [];
+
+        public int TotalSkins =>
+            Skins.Count;
+
+        public SkinDefinition? LowestSupply =>
+            Skins
+                .Where(x => x.SellOrderCount > 0)
+                .OrderBy(x => x.SellOrderCount)
+                .FirstOrDefault();
+
+
+        public SkinDefinition? HighestSupply =>
+            Skins
+                .Where(x => x.SellOrderCount > 0)
+                .MaxBy(x => x.SellOrderCount);
+    }
+}
