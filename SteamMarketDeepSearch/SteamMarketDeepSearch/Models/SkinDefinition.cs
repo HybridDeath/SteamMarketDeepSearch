0e3e28c4f58934183fdf474e047e269d1680fd28
HybridDeath <hyperek635@gmail.com>
2026-07-29 07:44:51 +0200

    Prepare for L1 dry test

--- SteamMarketDeepSearch/SteamMarketDeepSearch/Models/SkinDefinition.cs
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Models/SkinDefinition.cs
@@ -6,15 +6,11 @@ namespace SteamMarketDeepSearch.Models
     public class SkinDefinition
     {
         public long Id { get; set; }
-
         public WeaponType WeaponType { get; set; }
-
         public string MarketHashName { get; set; } = string.Empty;
-
         public string MarketBucketId { get; set; } = string.Empty;
-
         public int SellOrderCount { get; set; }
-
         public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
     }
 }
