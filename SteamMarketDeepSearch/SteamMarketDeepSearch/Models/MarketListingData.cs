a18d1e888a0f99975aeb97c3a1dc7ef503a1a014
HybridDeath <hyperek635@gmail.com>
2026-07-30 00:40:21 +0200

    Rename FloatValue to WearValue, change type to double

---
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Models/MarketListingData.cs
@@ -0,0 +1,15 @@
+﻿namespace SteamMarketDeepSearch.Models
+{
+    public class MarketListingData
+    {
+        public required string ListingId { get; set; }
+
+        public required string InspectLink { get; set; }
+
+        public int PaintSeed { get; set; }
+
+        public double WearValue { get; set; }
+
+        public int Price { get; set; }
+    }
+}
