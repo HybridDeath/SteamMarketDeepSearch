e9f00e4e318486750d27f60830500f23824c5cd5
HybridDeath <hyperek635@gmail.com>
2026-07-29 16:35:49 +0200

    Add L1 debug

--- SteamMarketDeepSearch/SteamMarketDeepSearch/Views/MainPage.xaml.cs
+++ SteamMarketDeepSearch/SteamMarketDeepSearch/Views/MainPage.xaml.cs
@@ -1,18 +1,8 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using SteamMarketDeepSearch.Models;
 using SteamMarketDeepSearch.Services;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,29 +14,44 @@ namespace SteamMarketDeepSearch.Views
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static readonly SteamMarketClient client =
+            new();
+
+        public static readonly WeaponCatalogService catalog =
+            new();
+
+        public static readonly SkinIndexerService indexer =
+            new(client, catalog);
+
         public MainPage()
         {
             InitializeComponent();
-
-            Loaded += MainPage_Loaded;
         }
 
-        private async void MainPage_Loaded(
-            object sender,
-            RoutedEventArgs e)
+        private async void IndexButton_Click(object sender, RoutedEventArgs e)
         {
-            Loaded -= MainPage_Loaded;
+            Debug.WriteLine("INDEX BUTTON CLICKED");
 
-            SteamMarketClient client =
-                new();
+            SkinIndexResult result =
+                await indexer.IndexSkinsAsync();
 
-            WeaponCatalogService catalog =
-                new();
 
-            SkinIndexerService indexer =
-                new(client, catalog);
+            LowestText.Text =
+                result.LowestSupply == null
+                ? "-"
+                :
+                $"{result.LowestSupply.MarketHashName}\n" +
+                $"Offers: {result.LowestSupply.SellOrderCount}";
 
-            await indexer.IndexSkinsAsync();
+
+            HighestText.Text =
+                result.HighestSupply == null
+                ? "-"
+                :
+                $"{result.HighestSupply.MarketHashName}\n" +
+                $"Offers: {result.HighestSupply.SellOrderCount}";
+
+            DebugText.Text = $"Total Skins Indexed: {result.TotalSkins}";
         }
     }
 }
